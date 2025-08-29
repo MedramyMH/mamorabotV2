31dd02c279f78b7f7f21c3f47c7b895f59ec7911
MedramyMH <151267442+MedramyMH@users.noreply.github.com>
2025-08-29 19:09:54 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TradingAssistant from './components/TradingAssistant';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <TradingAssistant />
+    </div>
+  );
+}
+
+export default App;
